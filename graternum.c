6181ec4711070ac292d72c16a72fc400bc345bc2
GOVIL K GOWDA <govilkgowda@gmail.com>
2024-12-12 07:18:48 +0530

    Create graternum.c

---
+++ graternum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int A,B;
+    printf("Enter two number \n");
+    scanf("%d%d",&A,&B);
+    if (A>B)
+    printf("A is GRATER");
+    else
+    printf("B is GRATER");
+    return 0;
+}
